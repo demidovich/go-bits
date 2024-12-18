a4ef2b900ca6687b7942e9ae12f72b0be3bb01a9
dm <dmitrij.demidovich@gmail.com>
2024-12-18 09:11:04 +0300

    feat: add worker pool

---
+++ concurrency/worker_pool/main.go
@@ -0,0 +1,47 @@
+// Вы разрабатываете сервис, который работает с изображениями
+// Каждое изображение проходит дорогостоящую обработку, например наложение водяного знака
+// Поскольку обработка каждого изображения занимает продолжительное время,
+// необходимо обрабатывать их параллельно, чтобы ускорить процесс
+// Однака, чтобы уменьшить нагрузку на систему вы хотите уменьшить количество
+// одновременно работающих горутин
+
+// Вывод результатов работы воркеров в канал string
+
+package main
+
+import (
+	"fmt"
+	"go-bits/concurrency/worker_pool/pool"
+	"strconv"
+
+	"github.com/google/uuid"
+)
+
+const (
+	workersCount = 10
+	tasksCount   = 100
+)
+
+func main() {
+
+	tasks := make(chan pool.Task)
+	results := pool.Start(workersCount, tasks)
+
+	// listen results
+	go func() {
+		for msg := range results {
+			fmt.Println(msg)
+		}
+	}()
+
+	// fake task generator
+	for i := 0; i < tasksCount; i++ {
+		task := pool.Task{
+			Id:   uuid.New(),
+			File: strconv.Itoa(i),
+		}
+		tasks <- task
+	}
+
+	close(tasks)
+}
---
+++ concurrency/worker_pool/pool/pool.go
@@ -0,0 +1,55 @@
+package pool
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Task struct {
+	Id   uuid.UUID
+	File string
+}
+
+func Start(workersCount int, tasks <-chan Task) <-chan string {
+	results := make(chan string)
+
+	wg := sync.WaitGroup{}
+	for num := 0; num < workersCount; num++ {
+		wg.Add(1)
+		go func(num int) {
+			defer wg.Done()
+			startWorker(num, tasks, results)
+		}(num)
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	return results
+}
+
+type worker struct {
+	num     int
+	results chan<- string
+}
+
+func startWorker(num int, tasks <-chan Task, results chan<- string) {
+	w := worker{
+		num:     num,
+		results: results,
+	}
+
+	for task := range tasks {
+		w.processImage(task)
+	}
+}
+
+func (w *worker) processImage(task Task) {
+	time.Sleep(time.Millisecond * 100)
+	w.results <- fmt.Sprintf("worker %d, task %s, file %s", w.num, task.Id, task.File)
+}
